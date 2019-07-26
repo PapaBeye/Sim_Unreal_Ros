bd25ed5226db5e83101879f20385c4f0130049dd
Papa Beye <papa.beye@navy.mil>
2019-07-26 14:53:27 -0400

    Initial Project Commit

---
+++ Intermediate/Build/Linux/B4D820EA/UE4Editor/Inc/TestRos/WAROSIntegrationGameInstance.gen.cpp
@@ -0,0 +1,84 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "TestRos/WAROSIntegrationGameInstance.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeWAROSIntegrationGameInstance() {}
+// Cross Module References
+	TESTROS_API UClass* Z_Construct_UClass_UWAROSIntegrationGameInstance_NoRegister();
+	TESTROS_API UClass* Z_Construct_UClass_UWAROSIntegrationGameInstance();
+	ROSINTEGRATION_API UClass* Z_Construct_UClass_UROSIntegrationGameInstance();
+	UPackage* Z_Construct_UPackage__Script_TestRos();
+// End Cross Module References
+	void UWAROSIntegrationGameInstance::StaticRegisterNativesUWAROSIntegrationGameInstance()
+	{
+	}
+	UClass* Z_Construct_UClass_UWAROSIntegrationGameInstance_NoRegister()
+	{
+		return UWAROSIntegrationGameInstance::StaticClass();
+	}
+	struct Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_UROSIntegrationGameInstance,
+		(UObject* (*)())Z_Construct_UPackage__Script_TestRos,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics::Class_MetaDataParams[] = {
+		{ "IncludePath", "WAROSIntegrationGameInstance.h" },
+		{ "ModuleRelativePath", "WAROSIntegrationGameInstance.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<UWAROSIntegrationGameInstance>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics::ClassParams = {
+		&UWAROSIntegrationGameInstance::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x001000A8u,
+		METADATA_PARAMS(Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_UWAROSIntegrationGameInstance()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(UWAROSIntegrationGameInstance, 3574846763);
+	template<> TESTROS_API UClass* StaticClass<UWAROSIntegrationGameInstance>()
+	{
+		return UWAROSIntegrationGameInstance::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_UWAROSIntegrationGameInstance(Z_Construct_UClass_UWAROSIntegrationGameInstance, &UWAROSIntegrationGameInstance::StaticClass, TEXT("/Script/TestRos"), TEXT("UWAROSIntegrationGameInstance"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(UWAROSIntegrationGameInstance);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ Intermediate/Build/Linux/B4D820EA/UE4Editor/Inc/TestRos/WAROSIntegrationGameInstance.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TESTROS_WAROSIntegrationGameInstance_generated_h
+#error "WAROSIntegrationGameInstance.generated.h already included, missing '#pragma once' in WAROSIntegrationGameInstance.h"
+#endif
+#define TESTROS_WAROSIntegrationGameInstance_generated_h
+
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_RPC_WRAPPERS
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUWAROSIntegrationGameInstance(); \
+	friend struct Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics; \
+public: \
+	DECLARE_CLASS(UWAROSIntegrationGameInstance, UROSIntegrationGameInstance, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/TestRos"), NO_API) \
+	DECLARE_SERIALIZER(UWAROSIntegrationGameInstance)
+
+
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesUWAROSIntegrationGameInstance(); \
+	friend struct Z_Construct_UClass_UWAROSIntegrationGameInstance_Statics; \
+public: \
+	DECLARE_CLASS(UWAROSIntegrationGameInstance, UROSIntegrationGameInstance, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/TestRos"), NO_API) \
+	DECLARE_SERIALIZER(UWAROSIntegrationGameInstance)
+
+
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UWAROSIntegrationGameInstance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UWAROSIntegrationGameInstance) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UWAROSIntegrationGameInstance); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UWAROSIntegrationGameInstance); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UWAROSIntegrationGameInstance(UWAROSIntegrationGameInstance&&); \
+	NO_API UWAROSIntegrationGameInstance(const UWAROSIntegrationGameInstance&); \
+public:
+
+
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UWAROSIntegrationGameInstance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UWAROSIntegrationGameInstance(UWAROSIntegrationGameInstance&&); \
+	NO_API UWAROSIntegrationGameInstance(const UWAROSIntegrationGameInstance&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UWAROSIntegrationGameInstance); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UWAROSIntegrationGameInstance); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UWAROSIntegrationGameInstance)
+
+
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_PRIVATE_PROPERTY_OFFSET
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_12_PROLOG
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_PRIVATE_PROPERTY_OFFSET \
+	TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_RPC_WRAPPERS \
+	TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_INCLASS \
+	TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_PRIVATE_PROPERTY_OFFSET \
+	TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_INCLASS_NO_PURE_DECLS \
+	TestRos_Source_TestRos_WAROSIntegrationGameInstance_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TESTROS_API UClass* StaticClass<class UWAROSIntegrationGameInstance>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TestRos_Source_TestRos_WAROSIntegrationGameInstance_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
